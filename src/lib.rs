a49ad657f0c31a35d7851e86b27165e4a03b9edc
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2025-12-04 09:37:24 -0700

    chore: add placeholder code

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,39 @@
+use unicode_segmentation::UnicodeSegmentation as _;
 
+/// Reverses a Unicode string
+pub fn reverse_unicode<S: AsRef<str>>(input: S) -> String {
+    input.as_ref().graphemes(true).rev().collect()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::reverse_unicode;
+
+    #[test]
+    fn ascii_reversed() {
+        let input = "hello";
+
+        let reversed = reverse_unicode(input);
+
+        assert_eq!(reversed, "olleh");
+    }
+
+    #[test]
+    fn utf8_single_character() {
+        let input = "h\u{00e9}llo";
+
+        let reversed = reverse_unicode(input);
+
+        assert_eq!(reversed, "oll\u{00e9}h");
+    }
+
+    #[test]
+    fn utf8_multi_character() {
+        let input = "he\u{0301}llo";
+
+        let reversed = reverse_unicode(input);
+
+        // the `\u{0301}` should remain after the `e`
+        assert_eq!(reversed, "olle\u{0301}h");
+    }
+}
